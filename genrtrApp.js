c3ed614a663eb2523c5aa207ce89cb82d8680da8
786-go <noorullahw@gmail.com>
2022-03-06 19:17:09 -0800

    html shell created

--- genrtrApp.js
+++ genrtrApp.js
@@ -1,10 +1,7 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-const generatePage = (name, github) => {
+const generatePage = () => {
   return `
   <!DOCTYPE html> 
   <html lang="en"> 
@@ -12,19 +9,55 @@ const generatePage = (name, github) => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
+    <title>Project READme</title>
   </head>
 
   <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
+
+    <h1>${'project title'}</h1>
+    <h3><a href="https://github.com/${'github'}">Github</a></h3>
+
+    <h3>Description</h3>
+
+    <section>
+    <h3>Table of Contents</h3>
+    <ul>
+        <li><a href="#install">Installation</a></li>
+        <li><a href="#usage">Usage</a></li>
+        <li><a href="#license">License</a></li>
+        <li><a href="#contribute">Contributing</a></li>
+        <li><a href="#tests">Tests</a></li>
+        <li><a href="#questions">Questions</a></li>
+
+    </section>
+
+    <h3 id="install">Installation</h3>
+
+    <h3 id="usage">Usage</h3>
+
+    <h3 id="license">License</h3>
+
+    <h3 id="contribute">Contributing</h3>
+
+    <h3 id="tests">Tests</h3>
+
+    <h3 id="questions">Questions</h3>
+
+
+
   </body>
   </html>
   `;
 };
-
-  fs.writeFile('index.html', generatePage(name, github), err => {
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+  fs.writeFile('index.html', generatePage(), err => {
     if (err) throw err;
   
     console.log('READme complete! Check out index.html to see the output!');
   });
+
+
+
+
+
+
